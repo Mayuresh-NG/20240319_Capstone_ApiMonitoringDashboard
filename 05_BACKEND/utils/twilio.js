1b33c4afcb54c2cd692b21178da174775d94d579
Mayuresh <mayureshngorantiwar@gmail.com>
2024-04-17 14:25:09 +0530

    folder dtructure for components improved

--- 05_BACKEND/utils/twilio.js
+++ 05_BACKEND/utils/twilio.js
@@ -1,7 +1,6 @@
 // twilioConfig.js used for sending alert to mobile Number
 module.exports = {
-    accountSid: 'AC4368006a31ce2206c8c4e614b97d5477',
-    authToken: 'cb9d684da80749fd6bda5e621913fb9d',
-    twilioNumber: '+12512659176'
-  };
-  
+  accountSid: "AC4368006a31ce2206c8c4e614b97d5477",
+  authToken: "eca3dcf48049fbb031512eec23fd7d65",
+  twilioNumber: "+12512659176",
+};
